6f13800d9591f3e449fd1afdc642173f570d1e0a
lbrionesm <liss.briones@gmail.com>
2024-02-07 07:42:25 -0300

    file updated

--- prog1.py
+++ prog1.py
@@ -2,3 +2,5 @@ Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 print ("help")
 help
+print ("new change")
+
